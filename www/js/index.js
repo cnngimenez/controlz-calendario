fa8492a795819ae6947cbf215eb9cf39c3af3814
cngimenez <cnngimenez@disroot.org>
2023-07-02 19:08:29 -0300

    Filtro para tipos en el calendario.

--- www/js/index.js
+++ www/js/index.js
@@ -17,6 +17,11 @@
   along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
+/**
+ Filtro para mostrar o ocutlar eventos del calendario.
+ */
+var mostrar_tipos = {};
+
 function configurar_scheduler() {
     // scheduler.config.readonly = true;
     scheduler.config.readonly_form = true;
@@ -25,6 +30,7 @@ function configurar_scheduler() {
     scheduler.i18n.setLocale("es");
     scheduler.plugins({
         tooltip: true,
+        recurring: true,
         readonly: true,
         agenda_view: true,
         year_view: true
@@ -37,7 +43,55 @@ function configurar_scheduler() {
             "<br/><b>Fin:</b> " + format(end);
     };
 
+    scheduler.filter_week = function (id, event) {
+        return mostrar_tipos[event.type];
+    };
+    scheduler.filter_day = function (id, event) {
+        return mostrar_tipos[event.type];
+    };
+    scheduler.filter_month = function (id, event) {
+        return mostrar_tipos[event.type];
+    };
+    scheduler.filter_year = function (id, event) {
+        return mostrar_tipos[event.type];
+    };
+    
     scheduler.init('scheduler', new Date(), "month");
+}
+
+function actualizar_filtro() {
+    scheduler.update_view();
+}
+
+/**
+ Obtener si un checkbox de filtro es filtrado o no.
+
+ @return True o false según lo que el usuario haya tildado.
+ */
+function get_filter(tipo) {
+    let elt = document.getElementById('chk-' + tipo);
+    
+    return elt.checked;
+}
+
+/**
+ Actualizar variable mostrar_tipos con los checkboxes.
+
+ La actualización es por side-effect. También se retorna.
+ 
+ @return La variable mostrar_tipos;
+ */
+function actualizar_mostrar_tipos() {
+    mostrar_tipos = {
+        radio: get_filter('radio'),
+        fai: get_filter('fai'),
+        fai_periodos: get_filter('fai-periodos'),
+        nacional: get_filter('nacional'),
+        cs: get_filter('cs'),
+        controlz: get_filter('controlz')
+    };
+    
+    return mostrar_tipos;
 }
 
 function obtener_datos(callback) {
@@ -56,9 +110,31 @@ function actualizar_datos(){
     });
 }
 
-function startup() {
+function on_chk_clicked(event) {
+    actualizar_mostrar_tipos();
+    actualizar_filtro();
+}
+
+function asignar_handlers() {
+    var chk = document.getElementById('chk-nacional');
+    chk.addEventListener('click', on_chk_clicked);
+    chk = document.getElementById('chk-cs');
+    chk.addEventListener('click', on_chk_clicked);
+    chk = document.getElementById('chk-fai');
+    chk.addEventListener('click', on_chk_clicked);
+    chk = document.getElementById('chk-fai-periodos');
+    chk.addEventListener('click', on_chk_clicked);
+    chk = document.getElementById('chk-radio');
+    chk.addEventListener('click', on_chk_clicked);
+    chk = document.getElementById('chk-controlz');
+    chk.addEventListener('click', on_chk_clicked);
+}
+
+function startup() {    
     configurar_scheduler();
+    actualizar_mostrar_tipos();
     actualizar_datos();
+    asignar_handlers();
 }
 
 if (document.readyState !== 'loading') {
